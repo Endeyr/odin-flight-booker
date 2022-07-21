25e81d5cdc3282e63847353ffd3131044c86acd9
Endeyr <aaron.thompson42594@gmail.com>
2022-07-21 11:36:34 -0400

    fixes for simple forms and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :airports
   resources :flights
   get 'find_order', :to => 'orders#find_order'
-  post :create_order, :to 'orders#create_order'
-  post :capture_order, :to 'orders#capture_order'
+  post :create_order, :to => 'orders#create_order'
+  post :capture_order, :to => 'orders#capture_order'
   mount Sidekiq::Web => "/sidekiq" if defined?(Sidekiq) # monitoring console
   root "flights#index"
 end
